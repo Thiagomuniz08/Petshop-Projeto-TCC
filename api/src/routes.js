ac3fe07098b9ea582300f332894db2c52d1286b3
ErickAguiar06 <erick.aguiar016@gmail.com>
2025-06-08 14:55:52 -0300

    atts

--- api/src/routes.js
+++ api/src/routes.js
@@ -3,6 +3,7 @@ const rota = express.Router();
 
 const usu = require('./controllers/usuario');
 const produto = require('./controllers/produto');
+const pedido = require('./controllers/pedido'); // ⬅️ Importa o novo controller
 
 // Rotas de usuário
 rota.post('/usuarios', usu.create);
@@ -11,5 +12,8 @@ rota.post('/login', usu.login);
 // Rotas de produto
 rota.post('/produtos', produto.create);
 rota.get('/produtos', produto.listar); 
+
+// Rota de pedido
+rota.post('/pedidos', pedido.create); // ⬅️ Nova rota adicionada
 
 module.exports = rota;
